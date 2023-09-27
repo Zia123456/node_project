d3b860d4cbdc5ef6a1bfc33ce2a3e3a3114794ce
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-27 07:11:13 -0700

    Create footer, navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world! This is Home</div>,
+    element: (
+      <div>
+        <Navbar />
+        <Home />
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/single',
+    element: <Single />,
+  },
+  {
+    path: '/write',
+    element: <Write />,
   },
 ]);
 

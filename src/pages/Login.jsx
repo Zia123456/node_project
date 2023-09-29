296df70ce12005ecc6daf7c8b9fcf135f814dcc3
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-09-29 09:34:45 -0700

    Create register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,6 +9,7 @@ function Login() {
         <input type="text" placeholder="username" />
         <input type="password" placeholder="password" />
         <button>Login</button>
+        <p>This is an error!</p>
         <span>
           Don't you have an account? <Link to="/register">Register</Link>
         </span>

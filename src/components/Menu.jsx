5f0e8afbacf4f31c0ce6b4a10fc1c26ec5897c46
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-10-05 11:14:46 -0700

    Map posts

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -31,6 +31,13 @@ function Menu() {
   return (
     <div className="menu">
       <h1>Other posts you may like</h1>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <img src={post.img} alt="Img" />
+          <h2>{post.title}</h2>
+          <button>Read More</button>
+        </div>
+      ))}
     </div>
   );
 }

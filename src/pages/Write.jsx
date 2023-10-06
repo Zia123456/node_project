756092f0f75ec05381619dd3dba38ac8d39610f4
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-10-06 11:14:48 -0700

    Create write content

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function Write() {
-  return <div>Write</div>;
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" />
+      </div>
+      <div className="menu">
+        <div className="item1">item1</div>
+        <div className="item2">item2</div>
+      </div>
+    </div>
+  );
 }
 
 export default Write;

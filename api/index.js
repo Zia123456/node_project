fe5bdd08c2ce7806a60658f93ce30eea8166db97
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-11-04 17:38:20 -0700

    Create post method for register

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import postRoutes from './routes/posts.js';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json()); // for parsing application/json
-
-app.get('/test', (req, res) => {
-  res.json('It works');
-});
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/posts', postRoutes);
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
